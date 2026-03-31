c531451e28f915896ad06168f454d707722edae8
Bowen Wang <iwangbowen@gmail.com>
2026-03-31 21:30:07 +0800

    chore(数据库变更): 移除不必要的文件存储配置字段 [实现: 删除ALTER TABLE语句，简化数据库结构]

--- packages/server/drizzle/0029_cuddly_havok.sql
+++ packages/server/drizzle/0029_cuddly_havok.sql
@@ -1,6 +1,4 @@
 CREATE TYPE "public"."message_channel" AS ENUM('email', 'sms', 'in_app');--> statement-breakpoint
-ALTER TYPE "public"."file_storage_provider" ADD VALUE 's3';--> statement-breakpoint
-ALTER TYPE "public"."file_storage_provider" ADD VALUE 'cos';--> statement-breakpoint
 CREATE TABLE "message_templates" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" varchar(100) NOT NULL,
@@ -15,14 +13,3 @@ CREATE TABLE "message_templates" (
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "message_templates_code_unique" UNIQUE("code")
 );
---> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "s3_region" varchar(64);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "s3_endpoint" varchar(256);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "s3_bucket" varchar(128);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "s3_access_key_id" varchar(128);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "s3_secret_access_key" varchar(256);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "s3_force_path_style" boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "cos_region" varchar(64);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "cos_bucket" varchar(128);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "cos_secret_id" varchar(128);--> statement-breakpoint
-ALTER TABLE "file_storage_configs" ADD COLUMN "cos_secret_key" varchar(256);
